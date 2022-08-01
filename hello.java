34cfcdb28101101b84b4f61283650f05a059e0d7
sfrodd <sfroddgit@git.edu>
2022-08-01 10:08:50 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,11 @@
 import java.lang.*;
-public static void Main(String args[])
-{
-  int a=5,b=7,c;
-  c=a+b;
-  System.out.println("Hello World!!! " + c);
-
+class Bank{
+ String bankName;
+ String bankAddress;
+ String city;
+ String zipCode;
+  
+   
+  
+  
 }
